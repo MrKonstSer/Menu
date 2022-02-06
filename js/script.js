8b840399f0a37518c7ee84c310ee87283f00d4d5
Konstantin <kuch.konst.ser@gmail.com>
2022-02-07 08:23:55 +1200

    Sliders

--- js/script.js
+++ js/script.js
@@ -200,36 +200,24 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    new MenuOnDay(
-        "img/tabs/vegy.jpg",
-        'vegy', 
-        'Меню "Фитнес"', 
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
-        1000, 
-        '.menu .container',
-        'menu__item'
-    ).madeHTML();
-    
-    new MenuOnDay(
-        "img/tabs/elite.jpg",
-        'elite', 
-        'Меню “Премиум”', 
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
-        14, 
-        '.menu .container',
-        'menu__item'
-    ).madeHTML();
 
-    new MenuOnDay(
-        "img/tabs/post.jpg",
-        'post', 
-        'Меню Меню "Постное"', 
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 
-        21, 
-        '.menu .container',
-        'menu__item'
-    ).madeHTML();
+    //Мы получаем с сервера, поэтому data нет, мы ничего не постим на сервер
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
+        if (!res.ok) {
+           throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json(); // трансформируем данные в js объект
+    };
+
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuOnDay(img, altimg, title, descr, price, '.menu .container').madeHTML();
+            });
+        });
 
     //Реализация скрипта отправки данных на сервер
 
@@ -246,10 +234,22 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData (form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data 
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData (form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -262,36 +262,23 @@ window.addEventListener('DOMContentLoaded', () => {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            // Как сделать так , чтобы все данные, которые заполнил пользователь в форме, мы получили в JS и уже могли отправить на сервер? Два формата: 1) FormData (убираем const object, убираем headers, в body пишем formData, в server.php комментим доп строку) 2) сейчас JSON 
 
-            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form); //Собираем все данные из нашей формы
 
-            // Как сделать так , чтобы все данные, которые заполнил пользователь в форме, мы получили в JS и уже могли отправить на сервер? Два формата: 1) FormData 2) JSON
+            const json = JSON.stringify(Object.fromEntries(formData.entries())); //Берем formData, ее сначала превращаем в массив массивов (entries), потом превращаем в классический объект (Object.fromEntries), а после этого - в объект json
 
-            const formData = new FormData(form);
-
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
+                console.log(data);  //data - те данные, которые вернул сервер
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
 
-            const json = JSON.stringify(object); //Уходит запрос с данными
-
-
-            request.send(json);
-
-            request.addEventListener('load', () => { //получаем ответ от сервера
-                if(request.status === 200) {  //если все ОК
-                    console.log(request.response); //то выводим в консоль результат
-                    showThanksModal(message.success); //запускаем функцию с сообщением , что все успешно. У нас покажется модальное окно и через 4 секунды оно будет закрываться, вместе с текстом и возвращением того нормального контента, который там будет
-                    form.reset(); // Чистка формы, чтобы все данные сбросились
-                    statusMessage.remove(); //Удаляем спиннер
-                    
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
         });
     }
 
@@ -321,27 +308,158 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-    //Fetch API вариант с GET запросом
 
-    /* fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())  // Этот метод превратит данные в формате JSON в обычный JS объект. response.json() - это Promise, и если все успешно прошло и он за какое то время преобразовался в объект - идет следующий then
-        .then(json => console.log(json)); // Здесь json - это объект JS  */
+    //Sliders
 
+    const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'),  
+          next = document.querySelector('.offer__slider-next'),
+          prev = document.querySelector('.offer__slider-prev'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+          
+    let slideIndex = 1;
+    let offset = 0;
 
-        //Fetch API вариант с POST запросом 
-        
-        fetch('https://jsonplaceholder.typicode.com/posts' , {
-            method : "POST",
-            body: JSON.stringify({name: "Alex"}),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        })
-        .then(response => response.json())  
-        .then(json => console.log(json));
-         
-        
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
 
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];  
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
+    function dotsToOpacity () {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
+
+    function currentAddZeroNot() {
+        if(slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    next.addEventListener('click', () => {
+        if(offset == deleteNotDigits(width) * (slides.length -1)) {  //'500px' переведем в число
+            offset = 0;
+        } else {
+            offset += deleteNotDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex ++;
+        }
+
+        currentAddZeroNot();
+
+        dotsToOpacity();
+    });
+
+    prev.addEventListener('click', () => {
+        if(offset == 0) {  //'650px' - ширина одного слайда переведем в число
+            offset = deleteNotDigits(width) * (slides.length -1); //это последний слайд
+        } else {
+            offset -= deleteNotDigits(width);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex --;
+        }
+
+        currentAddZeroNot();
+
+        dotsToOpacity();
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo -1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            currentAddZeroNot();
+
+            dotsToOpacity();
+        });
+    });
+    
 });  
  
 
