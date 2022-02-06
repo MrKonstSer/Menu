53199594a4849ad7be81cb37e39704db63058b5a
Konstantin <kuch.konst.ser@gmail.com>
2022-02-07 11:13:55 +1200

    Calc Part1

--- js/script.js
+++ js/script.js
@@ -459,7 +459,79 @@ window.addEventListener('DOMContentLoaded', () => {
             dotsToOpacity();
         });
     });
+
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female',
+        height, weight, age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if(!sex || !height || !weight || !age || !ratio) {
+            result.textContent = 'Не хватает данных, жирдяй';
+            return;  //return use чтобы прервать и не выполнять дальше программу
+        }
+
+        if(sex === 'female') {
+            result.textContent = Math.round((447.6 +(9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`); //Получим все элементы 
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {  //Если такой атрибут есть у объекта события(кнопки активности, на к. нажали)
+                    ratio = +e.target.getAttribute('data-ratio'); //То мы в переменную ratio записываем значение атрибута data-ratio. Вытаскиваем значение гетАттрибутом
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
     
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass);
+    
+                calcTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) { //Получаем id
+                case 'height':                 // Если это рост
+                    height = +input.value;     // Тогда рост = значению в кнопке
+                    break;                     // Выход и так далее
+                case 'weight':                 
+                    weight = +input.value;     
+                    break;
+                case 'age':                 
+                    age = +input.value;     
+                    break;                              
+            }
+
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
 });  
  
 
