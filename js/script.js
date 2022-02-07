c0152f46463dc574d38220be2f192818b80a9192
Konstantin <kuch.konst.ser@gmail.com>
2022-02-07 12:14:37 +1200

    Calc Part2

--- js/script.js
+++ js/script.js
@@ -464,13 +464,45 @@ window.addEventListener('DOMContentLoaded', () => {
     // Calc
 
     const result = document.querySelector('.calculating__result span');
-    let sex = 'female',
-        height, weight, age,
-        ratio = 1.375;
+
+    let sex, height, weight, age, ratio;
+
+    if(localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female');
+    }
+
+    if(localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = '1.375';
+        localStorage.setItem('ratio', '1.375');
+    }
+
+    // Ф-я, перебираем в ней элементы (пол или активность), если класс у элемента совпадает со значением атрибута у localStorage, то присваиваем этому элементу активный класс (зеленый)
+    function initLocalSettings (selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if(elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            }
+            if(elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettings('#gender div', 'calculating__choose-item_active');
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
 
     function calcTotal() {
         if(!sex || !height || !weight || !age || !ratio) {
-            result.textContent = 'Не хватает данных, жирдяй';
+            result.textContent = '_______';
             return;  //return use чтобы прервать и не выполнять дальше программу
         }
 
@@ -483,15 +515,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     calcTotal();
 
-    function getStaticInformation(parentSelector, activeClass) {
-        const elements = document.querySelectorAll(`${parentSelector} div`); //Получим все элементы 
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector); //Получим все элементы 
 
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {  //Если такой атрибут есть у объекта события(кнопки активности, на к. нажали)
                     ratio = +e.target.getAttribute('data-ratio'); //То мы в переменную ratio записываем значение атрибута data-ratio. Вытаскиваем значение гетАттрибутом
+                    localStorage.setItem('ratio', +e.target.getAttribute('data-ratio')); //Записываем в localStorage ключ и значение
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', e.target.getAttribute('id')); //Записываем в localStorage ключ и значение
                 }
     
                 elements.forEach(elem => {
@@ -505,13 +539,20 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    getStaticInformation('#gender', 'calculating__choose-item_active');
-    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
     
     function getDynamicInformation(selector) {
         const input = document.querySelector(selector);
 
-        input.addEventListener('input', () => {
+        input.addEventListener('input', () => { 
+
+            if(input.value.match(/\D/g)) {  //Если мы нашли какое то НЕ число в нашем value
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
             switch(input.getAttribute('id')) { //Получаем id
                 case 'height':                 // Если это рост
                     height = +input.value;     // Тогда рост = значению в кнопке
